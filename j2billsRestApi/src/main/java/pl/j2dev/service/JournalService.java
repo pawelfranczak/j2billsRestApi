40df8968316234b40e99ef4d5836bd6f9093689c
pfranczak <gzem89@gmail.com>
2017-10-16 22:03:42 +0200

    Add method createJournalMovement

--- j2billsRestApi/src/main/java/pl/j2dev/service/JournalService.java
+++ j2billsRestApi/src/main/java/pl/j2dev/service/JournalService.java
@@ -1,5 +1,6 @@
 package pl.j2dev.service;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import pl.j2dev.dao.jdbc.JournalDaoImpl;
 import pl.j2dev.pojo.Journal;
+import pl.j2dev.pojo.JournalMovement;
 
 @Service
 public class JournalService {
@@ -29,6 +31,40 @@ public class JournalService {
 		long id = dao.add(journal);
 		accountService.actualizeBalance(journal.getAccountId(), journal.getValue());
 		return dao.getById(id);
+	}
+	
+	/*
+	 * example of json
+	{
+	    "source": {
+	        "personId": 1,
+	        "accountId": 1,
+	        "value": 123,
+	        "description": "przelew"
+	    },
+	    "target": {
+	        "accountId": 2
+	    }
+	}
+	 */
+	
+	public JournalMovement createJournalMovement(JournalMovement jm) {
+		BigDecimal valueOfMovement = jm.getSource().getValue();
+		int personId = jm.getSource().getPersonId();
+		String description = jm.getSource().getDescription();
+		jm.getSource().setValue(valueOfMovement.negate());
+		jm.getTarget().setValue(valueOfMovement);
+		jm.getTarget().setPersonId(personId);
+		jm.getTarget().setDescription(description);
+		
+		Journal source = createJournalEntry(jm.getSource());
+		Journal target = createJournalEntry(jm.getTarget());
+		
+		JournalMovement jmNew = new JournalMovement();
+		jmNew.setSource(source);
+		jmNew.setTarget(target);
+		
+		return jmNew;
 	}
 	
 }
