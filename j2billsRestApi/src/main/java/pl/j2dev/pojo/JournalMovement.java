3ae3bffc95526a4f0f9d292293a58b5e37501956
pfranczak <gzem89@gmail.com>
2017-10-16 22:09:50 +0200

    New class used to make journal movement

---
+++ j2billsRestApi/src/main/java/pl/j2dev/pojo/JournalMovement.java
@@ -0,0 +1,27 @@
+package pl.j2dev.pojo;
+
+public class JournalMovement {
+
+	Journal source;
+	Journal target;
+	
+	public JournalMovement() {
+	}
+
+	public Journal getSource() {
+		return source;
+	}
+
+	public void setSource(Journal source) {
+		this.source = source;
+	}
+
+	public Journal getTarget() {
+		return target;
+	}
+
+	public void setTarget(Journal target) {
+		this.target = target;
+	}
+	
+}
